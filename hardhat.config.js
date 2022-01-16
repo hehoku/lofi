39d1109ad43c8eaa6c77e911a5adb990f9d5dcf9
Hehoku <hehoku@outlook.com>
2022-01-16 23:54:38 +0800

    fix #1

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,14 +7,6 @@ require('@nomiclabs/hardhat-ethers')
 
 const { API_URL, PRIVATE_KEY } = process.env
 
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners()
-
-  for (const account of accounts) {
-    console.log(account.address)
-  }
-})
-
 module.exports = {
   solidity: '0.8.9',
   defaultNetwork: 'polygon_mumbai',
@@ -22,9 +14,7 @@ module.exports = {
     hardhat: {},
     polygon_mumbai: {
       url: API_URL,
-      accounts: [`0x${PRIVATE_KEY}`],
-      gas: 100000,
-      gasPrice: 1000
+      accounts: [`0x${PRIVATE_KEY}`]
     }
   },
   mocha: {
